9f66569b95fce6fe76c8eb4073528fa973a92cf2
ChanThien3101 <chicanniemtin3101@gmail.com>
2024-05-16 00:07:48 +0700

    no edit secret text

--- ExWave.py
+++ ExWave.py
@@ -1,21 +1,20 @@
 
+
 import os
 import wave
 import argparse
 
-# Argument parser setup
 parser = argparse.ArgumentParser()
 parser.add_argument('-f', help='audiofile', dest='audiofile')
 args = parser.parse_args()
-
 af = args.audiofile
 
 def cls():
-    os.system("cls" if os.name == "nt" else "clear")
+    os.system("clear")
 
 def help():
-    print("\033[92mExtract Your Secret Message and File Name from Audio Wave File.\033[0m")
-    print('''usage: ExWave.py [-h] [-f AUDIOFILE]
+    print("\033[92mExtract Your Secret Message from Audio Wave File.\033[0m")
+    print ('''usage: ExWave.py [-h] [-f AUDIOFILE]
 
 optional arguments:
   -h, --help    show this help message and exit
@@ -41,28 +40,22 @@ def ex_msg(af):
         extracted = [frame_bytes[i] & 1 for i in range(len(frame_bytes))]
         binary_data = "".join(map(str, extracted))
         
-        # Find the end of the padding
-        pad_index = binary_data.find('0' * 8)
-        
-        # Extract the hidden message with file name
-        hidden_message = binary_data[:pad_index]
-        hidden_message = hidden_message[:-(len(hidden_message) % 8)]
+       
         
         # Convert binary data to characters
-        hidden_text = ''.join(chr(int(hidden_message[i:i+8], 2)) for i in range(0, len(hidden_message), 8))
+        hidden_text = "".join(chr(int("".join(map(str,extracted[i:i+8])),2)) for i in range(0,len(extracted),8))
         
         # Split hidden message into file name and content
-        file_name, file_content = hidden_text.split("\n", 1)
-        
-        # Remove padding characters
-        file_content = file_content.split('#')[0]
+        split_index = hidden_text.find("\n")
+        file_name = os.path.splitext(af)[0] + "_" + hidden_text[:split_index]
+        message_content = hidden_text[split_index + 1:].split("###")[0]
         
         # Write content to file with the same name as original file
-        output_file = os.path.splitext(af)[0] + "_" + file_name
-        with open(output_file, 'w') as file:
-            file.write(file_content)
+        with open(file_name, 'w') as file:
+            file.write(message_content)
         
-        print(f"Extracted file: {output_file}")
+        print(f"Extracted file: {file_name}")
+        print(f"Extracted message: {message_content}")
         
         # Close audio file
         waveaudio.close()
