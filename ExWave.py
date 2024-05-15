49581c5a1191c842f8c56c86dd01013cbee29054
ChanThien3101 <chicanniemtin3101@gmail.com>
2024-05-15 21:54:23 +0700

    edit hidden file

--- ExWave.py
+++ ExWave.py
@@ -1,49 +1,75 @@
-# HiddinWave Ver 1.0
-# Powered by TechChip
-# Secret Message Extractor
+
 import os
 import wave
 import argparse
+
+# Argument parser setup
 parser = argparse.ArgumentParser()
 parser.add_argument('-f', help='audiofile', dest='audiofile')
 args = parser.parse_args()
+
 af = args.audiofile
-arged = False
-if af:
-    arged = True
+
 def cls():
-  os.system("cls" if os.name == "nt" else "clear")
+    os.system("cls" if os.name == "nt" else "clear")
+
 def help():
-  print("\033[92mExtract Your Secret Message from Audio Wave File.\033[0m")
-  print ('''usage: ExWave.py [-h] [-f AUDIOFILE]
+    print("\033[92mExtract Your Secret Message and File Name from Audio Wave File.\033[0m")
+    print('''usage: ExWave.py [-h] [-f AUDIOFILE]
 
 optional arguments:
   -h, --help    show this help message and exit
   -f AUDIOFILE  Select Audio File''')
-  
+
 def banner():
-    print ('''
+    print('''
                          \033[1;91mHide secrets in audio\033[0m
                          \033[92mDinh Huu Duc 20NS\033[0m
                          \033[93mNguyen Viet Tan 20NS\033[0m''')
 
 def ex_msg(af):
-    if not arged:
-      help()
-    else:
-        print ("Please wait...")
+    if not af:
+        help()
+        return
+    
+    try:
+        print("Please wait...")
         waveaudio = wave.open(af, mode='rb')
         frame_bytes = bytearray(list(waveaudio.readframes(waveaudio.getnframes())))
+        
+        # Extract hidden message
         extracted = [frame_bytes[i] & 1 for i in range(len(frame_bytes))]
-        string = "".join(chr(int("".join(map(str,extracted[i:i+8])),2)) for i in range(0,len(extracted),8))
-        msg = string.split("###")[0]
-        print("Your Secret Message is: \033[1;91m"+msg+"\033[0m")
+        binary_data = "".join(map(str, extracted))
+        
+        # Find the end of the padding
+        pad_index = binary_data.find('0' * 8)
+        
+        # Extract the hidden message with file name
+        hidden_message = binary_data[:pad_index]
+        hidden_message = hidden_message[:-(len(hidden_message) % 8)]
+        
+        # Convert binary data to characters
+        hidden_text = ''.join(chr(int(hidden_message[i:i+8], 2)) for i in range(0, len(hidden_message), 8))
+        
+        # Split hidden message into file name and content
+        file_name, file_content = hidden_text.split("\n", 1)
+        
+        # Remove padding characters
+        file_content = file_content.split('#')[0]
+        
+        # Write content to file with the same name as original file
+        output_file = os.path.splitext(af)[0] + "_" + file_name
+        with open(output_file, 'w') as file:
+            file.write(file_content)
+        
+        print(f"Extracted file: {output_file}")
+        
+        # Close audio file
         waveaudio.close()
+        
+    except Exception as e:
+        print(f"Something went wrong: {e}")
+
 cls()
 banner()
-try:
-  ex_msg(af)
-except Exception as e:
-  print (f"Something went wrong: {e}")
-  print ("Something went wrong!! try again")
-  quit('')
+ex_msg(af)
--- HiddenWave.py
+++ HiddenWave.py
@@ -1,60 +1,86 @@
-# HiddinWave Ver 1.0
-# Powered by TechChip
-# Hide your secret text in wave audio file.
+
+# Hide your secret text and file name in wave audio file.
+
 import os
 import wave
 import argparse
+
+# Argument parser setup
 parser = argparse.ArgumentParser()
 parser.add_argument('-f', help='Select Audio File', dest='audiofile')
-parser.add_argument('-m', help='Enter your Secret Message', dest='secretmsg')
+parser.add_argument('-m', help='Enter your Secret Message or file txt', dest='secretmsg')
 parser.add_argument('-o', help='Your Output file path and name', dest='outputfile')
 args = parser.parse_args()
+
 af = args.audiofile
-string = args.secretmsg
+secret_msg = args.secretmsg
 output = args.outputfile
-arged = False
-if af and string and output:
-    arged = True
+
 def cls():
-  os.system("cls" if os.name == "nt" else "clear")
+    os.system("cls" if os.name == "nt" else "clear")
+
 def help():
-  print("\033[92mHide Your Secret Message in Audio Wave File.\033[0m")
-  print ('''usage: HiddenWave.py [-h] [-f AUDIOFILE] [-m SECRETMSG] [-o OUTPUTFILE]
+    print("\033[92mHide Your Secret Message and File Name in Audio Wave File.\033[0m")
+    print('''usage: HiddenWave.py [-h] [-f AUDIOFILE] [-m SECRETMSG] [-o OUTPUTFILE]
 
 optional arguments:
   -h, --help    show this help message and exit
   -f AUDIOFILE  Select Audio File
   -m SECRETMSG  Enter your message or file txt
   -o OUTPUTFILE Your output file path and name''')
-  
+
 def banner():
-    print ('''
+    print('''
                          \033[1;91mHide secrets in audio\033[0m
                          \033[92mDinh Huu Duc 20NS\033[0m
                          \033[93mNguyen Viet Tan 20NS\033[0m''')
 
-def em_audio(af, string, output):
-    if not arged:
-      help()
+def get_secret_message(input_str):
+    # Check if input_str is a file path
+    if os.path.isfile(input_str):
+        with open(input_str, 'r') as file:
+            # Return file name + file content
+            return os.path.basename(input_str) + "\n" + file.read()
     else:
-      print ("Please wait...")
-      waveaudio = wave.open(af, mode='rb')
-      frame_bytes = bytearray(list(waveaudio.readframes(waveaudio.getnframes())))
-      string = string + int((len(frame_bytes)-(len(string)*8*8))/8) *'#'
-      bits = list(map(int, ''.join([bin(ord(i)).lstrip('0b').rjust(8,'0') for i in string])))
-      for i, bit in enumerate(bits):
-        frame_bytes[i] = (frame_bytes[i] & 254) | bit
-      frame_modified = bytes(frame_bytes)
-      with wave.open(output, 'wb') as fd:
-        fd.setparams(waveaudio.getparams())
-        fd.writeframes(frame_modified)
-      waveaudio.close()
-      print ("Done...")
+        return input_str
+
+def em_audio(af, secret_msg, output):
+    if not (af and secret_msg and output):
+        help()
+        return
+    
+    try:
+        print("Please wait...")
+        waveaudio = wave.open(af, mode='rb')
+        frame_bytes = bytearray(list(waveaudio.readframes(waveaudio.getnframes())))
+        
+        # Get secret message with file name
+        secret_msg = get_secret_message(secret_msg)
+        
+        # Add padding to secret message
+        secret_msg = secret_msg + int((len(frame_bytes) - (len(secret_msg) * 8 * 8)) / 8) * '#'
+        
+        # Convert secret message to binary
+        bits = list(map(int, ''.join([bin(ord(i)).lstrip('0b').rjust(8, '0') for i in secret_msg])))
+        
+        # Embed secret message into audio frames
+        for i, bit in enumerate(bits):
+            frame_bytes[i] = (frame_bytes[i] & 254) | bit
+        
+        # Write modified frames to output file
+        frame_modified = bytes(frame_bytes)
+        with wave.open(output, 'wb') as fd:
+            fd.setparams(waveaudio.getparams())
+            fd.writeframes(frame_modified)
+        
+        # Close audio file
+        waveaudio.close()
+        
+        print("Done...")
+        
+    except Exception as e:
+        print(f"Something went wrong: {e}")
+
 cls()
 banner()
-try:
-  em_audio(af, string, output)
-except Exception as e:
-  print (f"Something went wrong: {e}")
-  print ("Something went wrong!! try again")
-  quit('')
+em_audio(af, secret_msg, output)
