30de95a55c0dbe1f490aac8443cde1c381073c39
ChanThien3101 <chicanniemtin3101@gmail.com>
2024-05-16 02:21:59 +0700

    no edit secret text

--- ExWave.py
+++ ExWave.py
@@ -10,7 +10,7 @@ args = parser.parse_args()
 af = args.audiofile
 
 def cls():
-    os.system("clear")
+    os.system("cls" if os.name == "nt" else "clear")
 
 def help():
     print("\033[92mExtract Your Secret Message from Audio Wave File.\033[0m")
@@ -32,6 +32,9 @@ def ex_msg(af):
         return
     
     try:
+        if not os.path.exists(af):
+            raise FileNotFoundError("Audio file not found.")
+
         print("Please wait...")
         waveaudio = wave.open(af, mode='rb')
         frame_bytes = bytearray(list(waveaudio.readframes(waveaudio.getnframes())))
@@ -40,22 +43,29 @@ def ex_msg(af):
         extracted = [frame_bytes[i] & 1 for i in range(len(frame_bytes))]
         binary_data = "".join(map(str, extracted))
         
-       
-        
         # Convert binary data to characters
         hidden_text = "".join(chr(int("".join(map(str,extracted[i:i+8])),2)) for i in range(0,len(extracted),8))
         
         # Split hidden message into file name and content
         split_index = hidden_text.find("\n")
-        file_name = os.path.splitext(af)[0] + "_" + hidden_text[:split_index]
-        message_content = hidden_text[split_index + 1:].split("###")[0]
         
-        # Write content to file with the same name as original file
-        with open(file_name, 'w') as file:
-            file.write(message_content)
+        print("in hidden text: " + hidden_text.split("###")[0])
+
+        if split_index == -1:
+            # If newline character is not found, directly print the hidden message
+            msg = hidden_text.split("###")[0]
+            print("Your Secret Message is: \033[1;94m"+msg+"\033[0m")
+        else:
         
-        print(f"Extracted file: {file_name}")
-        print(f"Extracted message: {message_content}")
+          file_name = os.path.splitext(af)[0] + "_" + hidden_text[:split_index]
+          message_content = hidden_text[split_index + 1:].split("###")[0]
+          
+          # Write content to file with the same name as original file
+          with open(file_name, 'w') as file:
+              file.write(message_content)
+          print(f"Extracted file: {file_name}")
+          print(f"Extracted message: {message_content}")
+          
         
         # Close audio file
         waveaudio.close()
